366e4ec14dd982d12e186fc5131a691d4849b5c7
kadillon21 <kadillon21@gmail.com>
2026-06-09 09:29:31 -0500

    Program can now read data from log file.

--- src/main/java/com/kendall/FileParser.java
+++ src/main/java/com/kendall/FileParser.java
@@ -1,4 +1,32 @@
 package com.kendall;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class FileParser {
+    private String ip;
+    private LocalDateTime timeStamp;
+    private String endPoint;
+    private int method;
+
+    public void loadLogFile() throws FileNotFoundException {
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/access.log");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String input;
+            while ((input = bufferedReader.readLine()) != null){
+                System.out.println(input);
+            }
+
+
+        } catch (FileNotFoundException e){
+            System.out.println("Error: " + e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
