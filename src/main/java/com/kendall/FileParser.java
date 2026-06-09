a02afb88f9a09472ac6d18e1c94e8eb79fb732bc
kadillon21 <kadillon21@gmail.com>
2026-06-09 10:46:01 -0500

    Program can now properly parse and save data to proper variables

--- src/main/java/com/kendall/FileParser.java
+++ src/main/java/com/kendall/FileParser.java
@@ -4,23 +4,40 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.CharacterIterator;
 import java.time.LocalDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileParser {
     private String ip;
-    private LocalDateTime timeStamp;
+    private String timeStamp;
     private String endPoint;
-    private int method;
+    private String method;
 
     public void loadLogFile() throws FileNotFoundException {
         try {
             FileReader fileReader = new FileReader("src/main/resources/access.log");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            String input;
-            while ((input = bufferedReader.readLine()) != null){
-                System.out.println(input);
+            String logEntry;
+            String regex = "^(\\S+) (\\S+) (\\S+) \\[(.*?)\\] \"(.*?) (.*?) (.*?)\" (\\d{3}) (\\S+) \"(.*?)\" \"(.*?)\"";
+            Pattern pattern = Pattern.compile(regex);
+
+            while ((logEntry = bufferedReader.readLine()) != null){
+                Matcher matcher = pattern.matcher(logEntry);
+
+                if (matcher.find()){
+                    ip = matcher.group(1);
+                    timeStamp = matcher.group(4);
+                    endPoint = matcher.group(6);
+                    method = matcher.group(5);
+                }
+                System.out.println("IP: " + ip + " Time Stamp: " + timeStamp + " End Point " + endPoint + " Method " + method);
+
+
             }
+
 
 
         } catch (FileNotFoundException e){
